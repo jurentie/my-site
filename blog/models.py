39bc68266e23320b80b443f3952e1d57cd9a1ce9
Justin Rentie <jurentie@gmail.com>
2025-12-16 11:50:04 -0700

    implemented SEO friendly URLs with canonical URL configuration

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 class PublishedManager(models.Manager):
@@ -17,7 +18,10 @@ class Post(models.Model):
         related_name='blog_posts'
     )
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(
+        max_length=250,
+        unique_for_date='publish'
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -38,3 +42,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug]
+        )
