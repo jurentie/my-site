869c2c73a028e4584b9d6c570be865928b7e154d
Justin Rentie <jurentie@gmail.com>
2025-12-18 15:16:35 -0700

    added comments and fixed pagination issue

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 
 from .models import Post
-from .forms import EmailPostForm
+from .forms import CommentForm, EmailPostForm
 
 
 # Clas Based View
@@ -42,11 +43,20 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day
     )
+
+    # list of comments
+    comments = post.comments.filter(active=True)
+    # form for users to comment
+    form = CommentForm()
     
     return render(
         request,
         'blog/post/detail.html',
-        {'post': post}
+        {
+            'post': post,
+            'comments': comments,
+            'form': form
+        }
     )
 
 def post_share(request, post_id):
@@ -93,5 +103,33 @@ def post_share(request, post_id):
             'post': post,
             'form': form,
             'sent': sent
+        }
+    )
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    comment = None
+    
+    # a comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment
         }
     )
